48f46b9aeb713bc18ff3be42ff2d9c875fefc0b3
Magnus Sverdrup <magger254@gmail.com>
2025-10-29 09:51:59 +0100

    New Tasks Included

--- js/main.js
+++ js/main.js
@@ -3,17 +3,52 @@ const app = Vue.createApp({
         return {
             intro: 'Welcome to my Vue template',
             hidden: false,
+            showLastname : true,
+            firstname : firstname,
+            lastname : lastname,
+            showStudent : true,
+            studentAge : true,
+            students : [
+                {"firstname" : "Magnus", "lastname" : "Asker", "age" : 29},
+                {"firstname" : "Vincent", "lastname" : "Arran", "age" : 25},
+                {"firstname" : "Kristoffer", "lastname" : "Einarson", "age" : 27}
+            ]
+            //students : ["Magnus", "Vincent", "Kristoffer"]
         }
     },
     methods: {
-        hide() {
+        hideName() {
             if (this.hidden == false) {
                 this.hidden = true
             }
         },
-        show() {
+        showName() {
             if (this.hidden == true) {
                 this.hidden = false
+            }
+        },
+        showStudents() {
+            if (this.showStudent) {
+                this.showStudent = false
+            }
+            else {
+                this.showStudent = true
+            }
+        },
+        showStudentAges() {
+            if (this.studentAge) {
+                this.studentAge = false
+            }
+            else {
+                this.studentAge = true
+            }
+        },
+        show() {
+            if (this.showLastname) {
+                this.showLastname = false
+            }
+            else {
+                this.showLastname = true
             }
         }
     },
